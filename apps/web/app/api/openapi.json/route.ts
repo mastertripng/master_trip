3142ee0d3bdcab36d461a62102de9b446247989d
moses <okonkwomoses158@gmail.com>
2026-07-12 09:35:32 +0100

    fix(web): force dynamic generation for openapi route

--- apps/web/app/api/openapi.json/route.ts
+++ apps/web/app/api/openapi.json/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { openAPI } from "@master-trip/api/openapi";
 
+export const dynamic = "force-dynamic";
+
 export async function GET(): Promise<NextResponse> {
   return NextResponse.json(openAPI);
 }
